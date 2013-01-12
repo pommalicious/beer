fd78aecf3617d35c60fea7bd2dec86cd5bbf44a9
Willy <willyh@outlook.com>
2013-01-11 18:34:09 -0800

    Added jsonify, comments

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 import requests
+import json
 from BeautifulSoup import BeautifulSoup
 app = Flask(__name__)
 
@@ -8,27 +9,43 @@ def get_beer():
 	search_url = "http://beeradvocate.com/search?qt=beer&q="+request.args['beer_name'].replace(" ","+")
 	r = requests.get(search_url)
 	soup = BeautifulSoup(r.text)
-	print soup.find(id="baContent").find("ul")
+	# print soup.find(id="baContent").find("ul")
 	links = soup.find(id="baContent").find("ul")
-	print "Hello"
-	print links
+	# print "Hello"
+	# print links
 	beer_url = "http://beeradvocate.com"+links.find("li").find("a").get('href')
-	print beer_url
+	# print beer_url
 	for li in links:
-		print "li ", li
+		# print "li ", li
 		link = li.find("a")
 		if link.find("b").text.upper() == request.args['beer_name'].upper():
 			beer_url = "http://beeradvocate.com"+link.get('href')
-	print beer_url
+	# print beer_url
 	#beer_url = "http://beeradvocate.com"+link
 	beer_id = beer_url.split("/")[-1]
-	print beer_id
+	# print beer_id
+
+	# Get beer image
 	image_url = "http://beeradvocate.com/im/beers/"+beer_id+".jpg"
 	beer_request = requests.get(beer_url)
 	beer_soup = BeautifulSoup(beer_request.text)
-	print beer_soup
+	# print beer_soup
+
+	# Get rating
 	rating = beer_soup.find("span","BAscore_big").text
-	return "<img src='"+image_url+"'/>\nrating: "+rating
+
+	# Create a dict
+	beer_info = {'image': image_url, 'rating': rating}
+
+	print beer_info
+
+	# Return JSON
+	return_json = jsonify(beer_info)
+	print return_json
+	#return_json = "blah"
+
+	return return_json
+	#return "<img src='"+image_url+"'/>\nrating: "+rating
 
 
 if __name__ == "__main__":
